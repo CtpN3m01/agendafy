0e69297826dfda92ccf5da2d320ff8e295141b09
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2025-06-07 01:18:22 -0600

    DTO Usuario y persona

---
+++ src/types/PersonaDTO.ts
@@ -0,0 +1,16 @@
+// src/types/PersonaDTO.ts
+export interface PersonaDTO {
+  nombre: string;
+  apellidos: string;
+  correo: string;
+  rol: 'Presidente' | 'SubPresidente' | 'Tesorero' | 'Vocal';
+  organizacion: string;
+}
+
+export interface CrearPersonaDTO {
+  nombre: string;
+  apellidos: string;
+  correo: string;
+  rol: 'Presidente' | 'SubPresidente' | 'Tesorero' | 'Vocal';
+  organizacion: string;
+}
